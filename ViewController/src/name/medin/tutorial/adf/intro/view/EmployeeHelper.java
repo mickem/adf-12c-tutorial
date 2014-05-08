602cde316f8731590ccbaa4891b03eca87c45904
Michael Medin <michael@medin.name>
2014-05-08 19:35:24 +0200

    13.3 Added action to managed bean

--- ViewController/src/name/medin/tutorial/adf/intro/view/EmployeeHelper.java
+++ ViewController/src/name/medin/tutorial/adf/intro/view/EmployeeHelper.java
@@ -4,4 +4,13 @@ public class EmployeeHelper {
     public EmployeeHelper() {
         super();
     }
+    private String action;
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getAction() {
+        return action;
+    }
 }
