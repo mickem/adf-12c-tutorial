fcd8d1aebe075425d7fcf61d3ccb90fee59c8a9c
Michael Medin <michael@medin.name>
2014-05-08 21:38:51 +0200

    15.1 Add view criteria to the employyes table

--- ViewController/src/name/medin/tutorial/adf/intro/view/EmployeeHelper.java
+++ ViewController/src/name/medin/tutorial/adf/intro/view/EmployeeHelper.java
@@ -1,5 +1,14 @@
 package name.medin.tutorial.adf.intro.view;
 
+import javax.faces.event.ValueChangeEvent;
+
+import oracle.adf.model.BindingContext;
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.adf.model.binding.DCIteratorBinding;
+
+import oracle.jbo.ViewCriteria;
+import oracle.jbo.ViewObject;
+
 public class EmployeeHelper {
     public EmployeeHelper() {
         super();
@@ -13,4 +22,14 @@ public class EmployeeHelper {
     public String getAction() {
         return action;
     }
+
+    public void ApplyCriteria(ValueChangeEvent valueChangeEvent) {
+        DCBindingContainer bindings = (DCBindingContainer) BindingContext.getCurrent().getCurrentBindingsEntry();
+        DCIteratorBinding dc = bindings.getIteratorBinding("EmployeesView", null, "EmployeesViewIterator");
+        ViewObject vo = dc.getViewObject();
+        ViewCriteria vc = vo.getViewCriteriaManager().getViewCriteria((String) valueChangeEvent.getNewValue());
+        dc.getViewObject().applyViewCriteria(vc);
+        dc.getViewObject().executeQuery();
+    }
+
 }
